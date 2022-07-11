6e43f1fa45452f610c3c8c9d9d64652fb3c49855
bonnaban <105739184+bonnaban@users.noreply.github.com>
2022-07-11 04:36:10 +0000

    [add]2-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,17 @@
 class BooksController < ApplicationController
   def new
   end
-  
+
   def create
     @book = Book.new(book_params)
-    
-    @book.save
-    
-    redirect_to book_path(@book.id)
+
+     if @book.save
+   flash[:notice] = 'Book was successfully created.'
+   redirect_to book_path(@book.id)
+ else
+   render :index
   end
+end
 
   def index
     @book=Book.new
@@ -16,29 +19,34 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id]) 
+    @book = Book.find(params[:id])
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)  
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+   flash[:no] = 'Book was successfully updated.'
+   redirect_to book_path(@book.id)
+ else
+   render :edit
   end
-  
+  end
+
   def destroy
-    @book = Book.find(params[:id])  
-    @book.destroy  
-    redirect_to '/books'  
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
-  
+
   private
-   
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
